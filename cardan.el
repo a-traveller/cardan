4d5980b2b5fa4234f725204b3d464b6c46ea6f53
atraveller <atraveller@protonmail.com>
2018-07-04 16:00:00 -0700

    In the beginning, there was Cardan

---
+++ cardan.el
@@ -0,0 +1,32 @@
+;;; cardan.el --- Define cardan commands   -*- lexical-binding: t; -*-
+;; Version: 0.1.0
+
+;; Copyright (C) 2018 Atraveller
+
+;; Author: Atraveller <atraveller@protonmail.com>
+;; Homepage: https://gitlab.com/atraveller
+
+;; This file is not part of GNU Emacs.
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published
+;; by the Free Software Foundation; either version 3 of the License,
+;; or (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU GPL see http://www.gnu.org/licenses.
+
+;;; Commentary:
+
+;; Define cardan commands.
+
+;;; Code:
+
+;;; _
+(provide 'cardan)
+;; End:
+;;; cardan.el ends here
